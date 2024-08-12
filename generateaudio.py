6857134ce2823ce1b65e5a9c5abd34f45d418d22
Nathan Birkett <98059854+NathanBirkett@users.noreply.github.com>
2024-08-12 09:11:20 -0400

    finally generated all of the audio files

--- generateaudio.py
+++ generateaudio.py
@@ -1,52 +1,52 @@
 import os
 from gtts import gTTS
 
-
-with open("out.txt", "r", encoding='utf-8') as f:
-    chapter = 2
-    section = 0
-    question = 0
-    part = -1
-    in_part = False
-    for line in f:
-        line = line[:-1]
-        if line == "<chap>":
-            # chapter += 1
-            if not os.path.exists("out/c{:02}".format(chapter)):
-                os.mkdir("out/c{:02}".format(chapter))
-        elif line == "</chap>":
-            sec = 0
-        elif line == "<sec>":
-            section += 1
-            if not os.path.exists("out/c{:02}/s{:02}".format(chapter, section)):
-                os.mkdir("out/c{:02}/s{:02}".format(chapter, section))
-        elif line == "</sec>":
-            question = 0
-            part = 0
-        elif line == "<part>":
-            part += 1
-            in_part = True
-            if not os.path.exists("out/c{:02}/s{:02}/p{:02}".format(chapter, section, part)):
-                os.mkdir("out/c{:02}/s{:02}/p{:02}".format(chapter, section, part))
-        elif line == "</part>":
-            question = 0
-            in_part = False
-        else:
-            question += 1
-            filename = ""
-            if not in_part:
-                filename = "out/c{:02}/s{:02}/q{:02}.mp3".format(chapter, section, question)
+for file in os.listdir("texts/out"):
+    with open("texts/out/{}".format(file), "r", encoding='utf-8') as f:
+        chapter = file.removesuffix(".txt")
+        section = 0
+        question = 0
+        part = -1
+        in_part = False
+        for line in f:
+            line = line[:-1]
+            if line == "<chap>":
+                # chapter += 1
+                if not os.path.exists("out/c{:02}".format(chapter)):
+                    os.mkdir("out/c{:02}".format(chapter))
+            elif line == "</chap>":
+                sec = 0
+            elif line == "<sec>":
+                section += 1
+                if not os.path.exists("out/c{:02}/s{:02}".format(chapter, section)):
+                    os.mkdir("out/c{:02}/s{:02}".format(chapter, section))
+            elif line == "</sec>":
+                question = 0
+                part = 0
+            elif line == "<part>":
+                part += 1
+                in_part = True
+                if not os.path.exists("out/c{:02}/s{:02}/p{:02}".format(chapter, section, part)):
+                    os.mkdir("out/c{:02}/s{:02}/p{:02}".format(chapter, section, part))
+            elif line == "</part>":
+                question = 0
+                in_part = False
             else:
-                filename = "out/c{:02}/s{:02}/p{:02}/q{:02}.mp3".format(chapter, section, part, question)
-            with open (filename, 'wb') as ff:
+                question += 1
+                filename = ""
                 if not in_part:
-                    print("{}-{}-{}".format(chapter, section, question))
+                    filename = "out/c{:02}/s{:02}/q{:02}.mp3".format(chapter, section, question)
                 else:
-                    print("{}-{}-{}-{}".format(chapter, section, part, question))
-                print(line)
-                try:
-                    tts = gTTS(text=line, lang="vi")
-                    # print(tts.text)
-                    tts.write_to_fp(ff)
-                except:
-                    print(question)
+                    filename = "out/c{:02}/s{:02}/p{:02}/q{:02}.mp3".format(chapter, section, part, question)
+                with open (filename, 'wb') as ff:
+                    if not in_part:
+                        print("{}-{}-{}".format(chapter, section, question))
+                    else:
+                        print("{}-{}-{}-{}".format(chapter, section, part, question))
+                    print(line)
+                    try:
+                        tts = gTTS(text=line, lang="vi")
+                        # print(tts.text)
+                        tts.write_to_fp(ff)
+                    except:
+                        print(question)
--- test.py
+++ test.py
@@ -1,2 +1,7 @@
-for num in range(0, 101):
-    print("num: {:02}".format(num))
+y = 0
+dy = 0.42
+for i in range(0, 60, 2):
+    y += dy * 0.02
+    dy -= 0.0318
+    dy *= 0.9920
+    print("{}: {}, {}".format(i, y, dy))
